18815f4d48e0fa002d951cffeaade96241934b27
Jennifer Herdzina <jen@Jennifers-MacBook-Pro.local>
2022-03-14 13:35:29 -0500

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -8,13 +8,13 @@ router.get('/', (req, res) => {
       city: 'Rockford',
       state: 'IL',
       cuisines: 'Nordic, Coffee, Cocktails',
-      pic:'https://d10j3mvrs1suex.cloudfront.net/u/274555/4c8d5f5f1d36d7bb2be141df1cb1bdd85c664b05/photo/1.jpg/!!/meta%3AeyJzcmNCdWNrZXQiOiJiemdsZmlsZXMifQ%3D%3D.jpg',
+      pic:'/images/Norweigian pic.jpeg',
     }, {
       name: "Jim & Patty's",
       city: 'Beaverton',
       state: 'OR',
       cuisines: 'Coffee, Tea, Breakfast Sandwiches',
-      pic: 'https://jim-pattys-coffee.square.site/uploads/1/3/5/1/135143057/s810439590534103056_p268_i2_w1920.jpeg?width=640',
+      pic: '/images/jim-pattys-breakfast-sammy.webp',
     }]
 
     res.render('places/index', { places })
--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,9 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
+              <div>
+                <img src="/images/Ebs-404.png" width={350} height={450} alt="Most adorable doggo with tortilla on her head"/>
+              </div>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
       </Def>
