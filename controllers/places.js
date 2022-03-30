d9db2824abc7b142e4a07bc5f7975a76f6b4803f
Jennifer Herdzina <jen@Jennifers-MacBook-Pro.local>
2022-03-29 20:15:35 -0500

    verified comments visable on show page

--- controllers/places.js
+++ controllers/places.js
@@ -39,8 +39,10 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
-      res.render('places/show', { place })
+    console.log(place.comments)
+    res.render('places/show', { place })
   })
   .catch(err => {
       console.log('err', err)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,26 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length>0) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+        //return comments;
+    }
     return (
         <Def>
           <main>
@@ -23,7 +43,7 @@ function show (data) {
                     Serving {data.place.cuisines}
                 </h4>
                 <h2>Comments</h2>
-                <p>No Comments Yet</p>
+                { comments }
             </div>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
@@ -40,4 +60,3 @@ function show (data) {
 }
 
 module.exports = show
-
